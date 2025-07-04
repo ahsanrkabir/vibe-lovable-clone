ac281e31228510dd9d049c7750f234489f2174a3
ahsanrkabir <ahsanrifat001@gamil.com>
2025-07-04 17:42:33 +0600

    04: background-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Client } from "./client";
+"use client";
 
-const Page = async () => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "Mavis" }));
+import { useMutation } from "@tanstack/react-query";
+
+import { Button } from "@/components/ui/button";
+
+import { useTRPC } from "@/trpc/client";
+import { toast } from "sonner";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job invoked successfully!");
+      },
+    })
+  );
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Client />
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ text: "Kabir" })}
+      >
+        Invoke Background Job
+      </Button>
+    </div>
   );
 };
 
